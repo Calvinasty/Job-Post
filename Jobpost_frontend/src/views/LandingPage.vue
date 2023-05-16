7a1827374a85975171f6517c516315cfc324abc3
Emmanuel Ofosu. Darko <Emmanuel Darko@maximnyansa.org>
2023-05-16 20:37:18 +0100

    create landing page header and nav feature

--- Jobpost_frontend/src/views/LandingPage.vue
+++ Jobpost_frontend/src/views/LandingPage.vue
@@ -1,23 +1,23 @@
 <template>
     <div>
         <PageLayout>
-
+            <component :is="headerComponent"></component>
         </PageLayout>
     </div>
 </template>
 
 <script>
 import PageLayout from '../components/PageLayout.vue';
+import HeaderComponent from '../components/HeaderComponent.vue';
 export default {
     components: {
         PageLayout,
-
-
+        HeaderComponent
     },
 
     data() {
         return {
-
+            headerComponent: HeaderComponent
         };
     },
 
