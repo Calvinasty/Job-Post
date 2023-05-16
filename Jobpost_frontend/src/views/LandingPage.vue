87b9714c359668c34bb3451992e515626be97103
Calvin <Calvinasty9@gmail.com>
2023-05-16 22:24:03 +0000

    update_fixed

--- Jobpost_frontend/src/views/LandingPage.vue
+++ Jobpost_frontend/src/views/LandingPage.vue
@@ -4,11 +4,11 @@
 
 
             <component :is="headerComponent"></component>
-             <component :is="regflow"></component>
+            <component :is="regflow"></component>
             <component :is="categories"></component>
             <component :is="jobPortal"></component>
             <component :is="testimonialsComponent"></component>
-             <component :is="footer"></component>
+            <component :is="footer"></component>
 
         </PageLayout>
     </div>
@@ -20,24 +20,24 @@ import TestimonialsComponent from '../components/TestimonialsComponent.vue'
 import FooterComponent from '../components/FooterComponent.vue';
 import PageLayout from '../components/PageLayout.vue';
 import RegFlow from '../components/RegFlow.vue';
-import CategorySection from '../components/CategorySection.vue'; 
-import JobPortalComponent from '@/components/JobPortalComponent.vue
+import CategorySection from '../components/CategorySection.vue';
+import JobPortalComponent from '@/components/JobPortalComponent.vue';
 
-      export default {
+export default {
     components: {
         PageLayout,
         HeaderComponent,
         RegFlow,
         CategorySection,
-         JobPortalComponent,
+        JobPortalComponent,
         TestimonialsComponent,
-         FooterComponent
-  },
+        FooterComponent
+    },
     data() {
         return {
             headerComponent: HeaderComponent,
-            regflow:'RegFlow',
-            categories:'CategorySection',
+            regflow: 'RegFlow',
+            categories: 'CategorySection',
             jobPortal: 'JobPortalComponent',
             testimonialsComponent: TestimonialsComponent,
             footer: 'FooterComponent'
