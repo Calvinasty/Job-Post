311d1ade60bafa5ff1bc563ecea3ba4e6eec36f2
zark28 <abdulrazakadams28@gmail.com>
2023-05-16 21:29:59 +0000

    create feature regFlow and feature category

--- Jobpost_frontend/src/views/LandingPage.vue
+++ Jobpost_frontend/src/views/LandingPage.vue
@@ -1,22 +1,27 @@
 <template>
     <div>
         <PageLayout>
-
+            <component :is="regflow"></component>
+            <component :is="categories"></component>
         </PageLayout>
     </div>
 </template>
 
 <script>
 import PageLayout from '../components/PageLayout.vue';
+import RegFlow from '../components/RegFlow.vue';
+import CategorySection from '../components/CategorySection.vue';
 export default {
     components: {
         PageLayout,
-
-
+        RegFlow,
+        CategorySection
     },
 
     data() {
         return {
+            regflow:'RegFlow',
+            categories:'CategorySection'
 
         };
     },
