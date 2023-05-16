ee725ddde7b3253ecd9e12da2a89196ff11f0ac1
Calvin <Calvinasty9@gmail.com>
2023-05-16 21:50:44 +0000

    feature_footer

--- Jobpost_frontend/src/views/LandingPage.vue
+++ Jobpost_frontend/src/views/LandingPage.vue
@@ -1,23 +1,25 @@
 <template>
     <div>
         <PageLayout>
-
+            <component :is="footer"></component>
         </PageLayout>
     </div>
 </template>
 
 <script>
+import FooterComponent from '../components/FooterComponent.vue';
 import PageLayout from '../components/PageLayout.vue';
 export default {
     components: {
         PageLayout,
+        FooterComponent
 
 
     },
 
     data() {
         return {
-
+            footer: 'FooterComponent'
         };
     },
 
