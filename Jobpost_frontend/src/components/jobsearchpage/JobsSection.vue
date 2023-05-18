9bf15e9261e5ed4959bf2dc0b2f37085adcbaa0e
zark28 <abdulrazakadams28@gmail.com>
2023-05-18 16:03:16 +0000

    create job search page layout

---
+++ Jobpost_frontend/src/components/jobsearchpage/JobsSection.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="jobs-listing">
+        jobs
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'JobPostJobsSection',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="css" scoped>
+ .jobs-listing{
+    background: #b83939;
+ }
+</style>
