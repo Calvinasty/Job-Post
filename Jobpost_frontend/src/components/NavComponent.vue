180611d253671d02970dc8c85c42b45a04aac8f8
Emmanuel Ofosu. Darko <Emmanuel Darko@maximnyansa.org>
2023-05-18 05:03:25 +0100

    mobile responsive design for header section of landing page

--- Jobpost_frontend/src/components/NavComponent.vue
+++ Jobpost_frontend/src/components/NavComponent.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="navbar"> 
         <div>
-            <img src="/images/logo.png" alt="Logo">
+            <router-link to="/"> <img src="/images/logo.png" alt="Logo"> </router-link>
         </div>
         <div class="auth-btns">
-            <button>Sign In</button>
-            <button>Sign Up</button>
+            <!-- <button>Sign In</button>
+            <button>Sign Up</button> -->
+            <router-link to="">Sign in</router-link>
+            <router-link to="">Sign up</router-link>
         </div>
     </div>
 </template>
@@ -22,16 +24,13 @@
         flex-direction: row;
         justify-content: space-between;
         align-items: center;
-        /* background-color: black; */
         width: 100%;
         color: #fff;
-        /* border: 2px solid #7FBF4C; */
     }
     .navbar div img{
         position: absolute;
         width: 150px;
         top:2%;
-        /* border: 1px solid blue */
     }
     .auth-btns{
         display: flex;
@@ -39,6 +38,8 @@
         justify-content: center;
         align-items: center;
         gap: 20px;
+        position: relative;
+        left: 0;
     }
     .auth-btns button{
         padding: 21px 45px;
@@ -48,6 +49,47 @@
         border-radius: 15px;
         cursor: pointer;
         font-size: 14px;
+    }
+
+    /* Mobile Media Queries*/
+    @media screen and (max-width: 480px){
+        .navbar{
+            display: inline-flex;
+            width: 100vdw;
+            /* border: 1px solid red; */
+            position: relative;
+            left: 0;
+            padding: 30px;
+        }
+        .navbar div img{
+            position:fixed;
+            width: 120px;
+            top:2.5%;
+            left: 0%;
+        }
+        .auth-btns{
+            display: flex;
+            width: 100%;
+            height: 20px;
+            flex-direction: row;
+            justify-content: end;
+            align-items: center;
+            gap: 10px;
+            /* border: 1px solid blue; */
+        }
+        .auth-btns a{
+            color: #fff;
+            text-decoration: none;
+            font-size: large;
+            margin-left: 20px;
+            text-decoration: underline;
+        }
+        .auth-btns a:hover{
+            background-color: #7FBF4C;
+            padding: 10px;
+            border-radius: 10px;
+            text-decoration: none;
+        }
     }
 
 </style>
