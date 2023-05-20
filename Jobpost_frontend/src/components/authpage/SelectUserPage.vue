e1ab683bd15402559297c45bacd3280452c4e46f
Alfred Ayertey <Alfred@maximnyansa.org>
2023-05-20 01:53:34 +0000

    Working on select user page

---
+++ Jobpost_frontend/src/components/authpage/SelectUserPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="box">
+        <SelectUserMobile />
+    </div>
+</template>
+
+<script>
+import SelectUserMobile from './SelectUserMobile.vue';
+    export default {
+        components: {
+            SelectUserMobile,
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
