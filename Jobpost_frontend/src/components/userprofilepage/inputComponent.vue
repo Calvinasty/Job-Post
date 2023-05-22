72c38246839625d6f02df1c3c39c977cec543fcf
Calvin <Calvinasty9@gmail.com>
2023-05-22 02:37:17 +0000

    updates from user profile

---
+++ Jobpost_frontend/src/components/userprofilepage/inputComponent.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="input-field">
+        <label :for="inputId">{{ fullName }}</label>
+        <input :type="inputType" :name="inputName" :id="inputId">
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+
+    props: [
+        "fullName",
+        "inputType",
+        "inputId",
+        "inputName"
+
+
+    ]
+
+}
+</script>
+
+<style lang="css" scoped>
+.input-field {
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    width: 100%;
+    gap: 10px;
+}
+
+.input-field label {
+    font-weight: 500;
+    font-size: 15px;
+    color: #898989;
+
+}
+
+.input-field input {
+    font-weight: 600;
+    font-size: 16px;
+    color: #000000;
+    border: none;
+    outline: none;
+    border-bottom: 2px solid #D9D9D9;
+
+}
+</style>
+
