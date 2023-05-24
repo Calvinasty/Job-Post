39b9d6a5966c86f6a7755349b2d7f10fb4d74b05
zark28 <abdulrazakadams28@gmail.com>
2023-05-24 17:56:56 +0000

    create data store

---
+++ Jobpost_frontend/src/data/categorySectionData.js
@@ -0,0 +1,45 @@
+ const categoryCardData=[
+    {
+        icon:'images/headset_icon.svg',
+        headText:'Technical Support',
+        subText:'59 Jobs Available'
+    },
+    {
+        icon:'images/code_icon.svg',
+        headText:'Software Development',
+        subText:'59 Jobs Available'
+    },
+    {
+        icon:'images/code_icon.svg',
+        headText:'Software Development',
+        subText:'59 Jobs Available'
+    },
+    {
+        icon:'images/code_icon.svg',
+        headText:'Software Development',
+        subText:'59 Jobs Available'
+    },
+    {
+        icon:'images/code_icon.svg',
+        headText:'Software Development',
+        subText:'59 Jobs Available'
+    },
+    {
+        icon:'images/code_icon.svg',
+        headText:'Software Development',
+        subText:'59 Jobs Available'
+    },
+    {
+        icon:'images/code_icon.svg',
+        headText:'Software Development',
+        subText:'59 Jobs Available'
+    },
+    {
+        icon:'images/code_icon.svg',
+        headText:'Software Development',
+        subText:'59 Jobs Available'
+    },
+    
+]
+export default categoryCardData
+
