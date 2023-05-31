fc229e6d14efdb34907a370ee44b75e8caa88e7c
Alfred Ayertey <Alfred@maximnyansa.org>
2023-05-31 03:30:22 +0000

    Company profile

---
+++ Jobpost_frontend/src/components/profilepage/updatecompany/UploadPicture.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="upload-picture">
+        <div>
+            <img src="" alt="" class="display-image">
+        </div>
+
+        <form action="" class="image-field">
+            <InputComponent inputType="file" accept="image/*" id="images-input" />
+        </form>
+    </div>
+</template>
+
+<script>
+    import  InputComponent from '../inputComponent.vue'
+    export default {
+        components: {
+             InputComponent
+        },
+
+        data() {
+            return {
+                image: ""
+            
+        }
+    }
+
+    }
+</script>
+
+<style lang="css" scoped>
+    .upload-picture {
+        display: flex;
+        justify-content: center;
+        align-content: center;
+        width: 40%;
+    }
+</style>
