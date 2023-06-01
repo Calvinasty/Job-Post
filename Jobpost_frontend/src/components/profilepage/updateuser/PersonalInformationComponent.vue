9cb583ac41b806bb1dd4b42f86bcc40641bd8bd0
Calvin <Calvinasty9@gmail.com>
2023-06-01 18:44:14 +0000

    updateprofile states

--- Jobpost_frontend/src/components/profilepage/updateuser/PersonalInformationComponent.vue
+++ Jobpost_frontend/src/components/profilepage/updateuser/PersonalInformationComponent.vue
@@ -1,36 +1,37 @@
 <template>
     <div class="card-detail">
         <div class="input-container">
-            <EditInputComponent inputName="First Name" inputType="text" />
+            <EditInputComponent inputName="First Name" name="first_name" inputType="text" :handleChange="handleInput()" />
         </div>
         <div class="input-container flex-center-row double">
-            <EditInputComponent inputName="Middle Name" inputType="text" />
-            <EditInputComponent inputName="Last Name" inputType="text" />
+            <EditInputComponent inputName="Middle Name" name="middle_name" inputType="text" :handleChange="handleInput()" />
+            <EditInputComponent inputName="Last Name" name="last_name" inputType="text" :handleChange="handleInput()" />
         </div>
         <div class="input-container">
-            <EditInputComponent inputName="Email" inputType="email" />
+            <EditInputComponent inputName="Email" name="email" inputType="email" :handleChange="handleInput()" />
         </div>
         <div class="input-container">
-            <EditInputComponent inputName="Contact" inputType="tel" />
+            <EditInputComponent inputName="Contact" name="contact" inputType="tel" :handleChange="handleInput()" />
         </div>
         <div class="input-container flex-center-row double">
-            <EditInputComponent inputName="LinkedIn URl" inputType="text" />
-            <EditInputComponent inputName="GitHub URL" inputType="text" />
+            <EditInputComponent inputName="LinkedIn URl" name="linkedIn_url" inputType="text"
+                :handleChange="handleInput()" />
+            <EditInputComponent inputName="GitHub URL" name="git_url" inputType="text" :handleChange="handleInput()" />
         </div>
         <div class="input-container">
-            <EditInputComponent inputName="Date of Birth" inputType="date" />
+            <EditInputComponent inputName="Date of Birth" name="dob" inputType="date" :handleChange="handleInput()" />
         </div>
         <div class="input-container">
-            <EditInputComponent inputName="Resume CV" inputType="" />
+            <EditInputComponent inputType="file" name="resume_cv" inputName="Resume CV" :handleChange="handleInput()" />
         </div>
         <div class="input-container flex-center-row gender">
             <p>Gender :</p>
             <span>
-                <input type="radio" name="gender" id="male">
+                <input type="radio" name="gender" id="male" :handleChange="handleInput()">
                 <label for="male">Male</label>
             </span>
             <span>
-                <input type="radio" name="gender" id="female">
+                <input type="radio" name="gender" id="female" :handleChange="handleInput()">
                 <label for="female">Female</label>
             </span>
 
@@ -49,8 +50,85 @@ export default {
 
     data() {
         return {
+            personalInfo: {
+                first_name: '',
+                middle_name: '',
+                last_name: '',
+                email: '',
+                contact: '',
+                linkedIn_url: '',
+                git_url: '',
+                dob: '',
+                resume_cv: '',
+                gender: '',
+            }
         }
     },
+
+    methods: {
+        handlePersonalInfo() {
+            const userPersonalInfo = {
+                first_name: this.personalInfo.first_name,
+                middle_name: this.personalInfo.middle_name,
+                last_name: this.personalInfo.last_name,
+                email: this.personalInfo.email,
+                contact: this.personalInfo.contact,
+                linkedIn_url: this.personalInfo.linkedIn_url,
+                git_url: this.personalInfo.git_url,
+                dob: this.personalInfo.dob,
+                resume_cv: this.personalInfo.resume_cv,
+                gender: this.personalInfo.gender,
+            }
+
+            console.log(userPersonalInfo)
+
+        },
+        handleInput(data) {
+            if (data.inputName == 'first_name') {
+                this.personalInfo.first_name = data.inputValue
+
+            }
+            if (data.inputName == 'middle_name') {
+                this.personalInfo.middle_name = data.inputValue
+
+            }
+            if (data.inputName == 'last_name') {
+                this.personalInfo.last_name = data.inputValue
+
+            }
+            if (data.inputName == 'email') {
+                this.personalInfo.email = data.inputValue
+
+            }
+            if (data.inputName == 'contact') {
+                this.personalInfo.contact = data.inputValue
+
+            }
+            if (data.inputName == 'linkedIn_url') {
+                this.personalInfo.linkedIn_url = data.inputValue
+
+            }
+            if (data.inputName == 'git_url') {
+                this.personalInfo.git_url = data.inputValue
+
+            }
+            if (data.inputName == 'dob') {
+                this.personalInfo.dob = data.inputValue
+
+            }
+            if (data.inputName == 'resume_cv') {
+                this.personalInfo.resume_cv = data.inputValue
+
+            }
+            if (data.inputName == 'gender') {
+                this.personalInfo.gender = data.inputValue
+
+            }
+
+            console.log(this.personalInfo)
+
+        }
+    }
 }
 </script>
 
---
+++ Jobpost_frontend/src/stores/updateuserprofile.js
@@ -0,0 +1,16 @@
+import { defineStore } from "pinia";
+
+export const useUpdateUserProfileStore = defineStore('updateuserprofile', {
+    state:() => ({
+
+        personalInfo:{}
+
+    }),
+
+    actions:{
+        
+    }
+
+    
+
+}) 
