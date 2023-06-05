b082f25242226c3a59d0416d9d5e338f9d30ce88
Calvin <Calvinasty9@gmail.com>
2023-06-05 10:58:58 +0000

    removed old frontend dir from main

--- Jobpost_frontend/src/data/index.js
+++
@@ -1,7 +0,0 @@
-import categoryCardData  from "./categorySectionData";
-import userprofileData from "./userprofileData";
-import jobsPosted from "./jobsPosted";
-
-
-
-export{categoryCardData,userprofileData,jobsPosted}
