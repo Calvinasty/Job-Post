8b19cee96622b6e6a78a525badbbf41d4e8b6dcd
Emmanuel Ofosu. Darko <Emmanuel Darko@maximnyansa.org>
2023-06-07 15:57:49 +0100

    Testing github eslint actions

--- Jobpost_frontend/src/components/companyDashboard/postjob/PostNav.vue
+++ Jobpost_frontend/src/components/companyDashboard/postjob/PostNav.vue
@@ -2,7 +2,7 @@
     <div>
         <ul>
             <router-link to="" v-for="(navItem, index) in navItems" @click="setNav(index)"
-                :class="{ active: navItem?.active }" :key="index">
+                :class="{ active: navItem?.active }">
                 <li>
                     {{ navItem.navName }}
                 </li>
