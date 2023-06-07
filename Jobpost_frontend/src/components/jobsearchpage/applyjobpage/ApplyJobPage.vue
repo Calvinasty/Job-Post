b63262ca23ab28a4c710d8963bb1c91b5934f0a0
zark28 <abdulrazakadams28@gmail.com>
2023-06-07 23:07:40 +0000

    resolve jobsearch-navbar over flow

--- Jobpost_frontend/src/components/jobsearchpage/applyjobpage/ApplyJobPage.vue
+++ Jobpost_frontend/src/components/jobsearchpage/applyjobpage/ApplyJobPage.vue
@@ -1,5 +1,4 @@
-<template>
-    
+<template>  
         <section class="job-container flex-center-row">
            <JobDescription :job="currentJob"/>
            <ApplyJobSection :user="currentUser"/>
@@ -8,12 +7,12 @@
 
 <script>
 
-import { useJobsStore } from '../../stores/Jobs';
-import { useUserStore } from '../../stores/users';
+import { useJobsStore } from '../../../stores/Jobs';
+import { useUserStore } from '../../../stores/users';
 import { mapState } from 'pinia';
 import { useRoute } from 'vue-router';
-import JobDescription from '../applyjobpage/JobDescription.vue';
-import ApplyJobSection from '../applyjobpage/ApplyJobSection.vue';
+import JobDescription from './JobDescription.vue';
+import ApplyJobSection from './ApplyJobSection.vue';
 // import JobCard from '../components/jobsearchpage/JobCard.vue';
 export default {
   components: { JobDescription, ApplyJobSection },
@@ -30,17 +29,16 @@ export default {
         ...mapState(useJobsStore,['postedJobs']),
         ...mapState(useUserStore,[''])
     },
-    mounted(){
-        this.handleJobDescription()
+    beforeMount(){
+        const route =useRoute()
+           const {jobId}= route.params
+        console.log(jobId);
+        this.handleJobDescription(jobId)
 
     },
     methods: {
-        handleJobDescription(){
-            const route =useRoute()
-           const {jobId}= route.params
-
-           console.log(this.postedJobs);
-            this.currentJob=this.postedJobs.filter(job=>(job.id==jobId))
+        handleJobDescription(jobId){
+            this.currentJob= this.postedJobs?.filter(job=>(job.id==jobId))
             console.log(this.currentJob);
         }   
         
@@ -49,18 +47,13 @@ export default {
 </script>
 
 <style lang="css" scoped>
-.apply-job-page{
-    row-gap:116px;
-    justify-content: flex-start;
-}
 .job-container{
     width: 100%;
     background: #286a439c;
     padding: 0px 94px;
     align-items: flex-start;
     justify-content: flex-start;
-    gap:41px
-
+    gap:41px;
 }
 
 </style>
