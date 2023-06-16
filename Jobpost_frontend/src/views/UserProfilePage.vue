c593ae6c940b028db07cd78831c981720c1db091
Calvin <Calvinasty9@gmail.com>
2023-06-16 10:00:59 +0000

    User profile page

--- Jobpost_frontend/src/views/UserProfilePage.vue
+++ Jobpost_frontend/src/views/UserProfilePage.vue
@@ -15,7 +15,7 @@
                     <div class="jobsapplied">
                         <div class="jobsapplied-1">
                             <img src="images/user_profile_icon.svg" alt="">
-                            <p>Jobs Applied</p>
+                            <router-link to="/jobSearch">Available Jobs</router-link>
                         </div>
                         <!-- <div class="jobsapplied-1">
                             <img src="images/user_profile_icon.svg" alt="">
@@ -26,7 +26,7 @@
                 <div class="profile-details">
                     <CardInformationComponent :key="key" :userValue="Value" :showPopup="showPopup" index="1"
                         :detailsTitle="inputCardDetails[0].cardTitle"
-                        :inputInformation="inputCardDetails[0].cardInputInformation" :showDetails="true"
+                        :inputInformation="inputCardDetails[0].cardInputInformation" :cardName="Value" :showDetails="true"
                         showpencil="showpencil">
                         <InputComponent :fullName="inputCardDetails[0].cardInputInformation.inputOne.name"
                             :inputType="inputCardDetails[0].cardInputInformation.inputOne.type" :Value="Value.fullname"
@@ -49,17 +49,17 @@
                             :inputId="inputCardDetails[0].cardInputInformation.inputEight.id" />
                     </CardInformationComponent>
                     <CardInformationComponent :userValue="Value" :showPopup="showPopup" index="2"
-                        :detailsTitle="inputCardDetails[1].cardTitle" :showplus="true">
+                        :detailsTitle="inputCardDetails[1].cardTitle" :cardName="user.education" :showplus="true">
                         <InputComponent v-for="(school, schoolIndex) in user.education" :key="schoolIndex"
                             :fullName="inputCardDetails[1].cardInputInformation.inputOne.name"
                             :inputType="inputCardDetails[1].cardInputInformation.inputOne.type"
                             :inputId="inputCardDetails[1].cardInputInformation.inputOne.id" :Value="school?.institution"
-                            :showpencil="true" :itemId="school.id" :handleEdit="handleEdit" :itemIndex="index" />
-
+                            :showpencil="true" :itemId="school.id" :itemIndex="index" />
+                        <!-- :handleEdit="handleEdit" -->
 
                     </CardInformationComponent>
                     <CardInformationComponent :userValue="Value" :showPopup="showPopup" index="3"
-                        :detailsTitle="inputCardDetails[2].cardTitle" :showplus="true">
+                        :detailsTitle="inputCardDetails[2].cardTitle" :cardName="user.experiences" :showplus="true">
                         <InputComponent v-for="(workexp, index) in user.experiences" :key="index"
                             :fullName="inputCardDetails[2].cardInputInformation.inputOne.name"
                             :inputType="inputCardDetails[2].cardInputInformation.inputOne.type"
@@ -68,7 +68,7 @@
 
                     </CardInformationComponent>
                     <CardInformationComponent :userValue="Value" :showPopup="showPopup" index="4"
-                        :detailsTitle="inputCardDetails[3].cardTitle" :showplus="true">
+                        :detailsTitle="inputCardDetails[3].cardTitle" :cardName="user.Skills" :showplus="true">
                         <InputComponent v-for="(skill, index) in user.Skills" :key="index"
                             :fullName="inputCardDetails[3].cardInputInformation.inputOne.name"
                             :inputType="inputCardDetails[3].cardInputInformation.inputOne.type"
@@ -98,9 +98,9 @@ import UpdateProfileComponentVue from '../components/profilepage/UpdateProfileCo
 import CardInformationComponent from '../components/profilepage/CardInformationComponent.vue';
 // import EditPopups from '../components/profilepage/EditPopups.vue';
 import InputComponent from '../components/profilepage/inputComponent.vue';
-// const BASE_URL = import.meta.env.VITE_BASE_URL
+const BASE_URL = import.meta.env.VITE_BASE_URL
 import { userprofileData } from '../data';
-// import axios from 'axios';
+import axios from 'axios';
 export default {
     components: {
         FooterComponent,
@@ -157,7 +157,7 @@ export default {
             this.Value.workexp = userInfo?.experiences
             this.title = this.Value.fullname
 
-            // this.getAllUserInfo()
+            this.getAllUserInfo()
         }
     },
     mounted() {
@@ -175,24 +175,24 @@ export default {
             this.index = index
         },
 
-        handleEdit(itemId, itemIndex) {
-            this.showPopup(itemIndex)
+        // handleEdit(itemId, itemIndex) {
+        //     this.showPopup(itemIndex)
 
-        },
-        // getAllUserInfo() {
-        //     const token = JSON.parse(localStorage.getItem('userToken'))
-        //     if (token) {
+        // },
+        getAllUserInfo() {
+            const token = JSON.parse(localStorage.getItem('userToken'))
+            if (token) {
 
-        //         axios.get(`${BASE_URL}/jobSeeker/getAllInfo`, { headers: { token } })
-        //             .then((res) => {
-        //                 localStorage.setItem("userDetails", JSON.stringify(res.data[0]))
-        //                 // console.log(res.data[0]);
-        //             })
-        //             .catch((err) => console.log(err))
-        //     }
+                axios.get(`${BASE_URL}/jobSeeker/getAllInfo`, { headers: { token } })
+                    .then((res) => {
+                        localStorage.setItem("userDetails", JSON.stringify(res.data[0]))
+                        // console.log(res.data[0]);
+                    })
+                    .catch((err) => console.log(err))
+            }
 
 
-        // }
+        }
 
 
 
@@ -289,10 +289,12 @@ export default {
     flex-direction: row;
 }
 
-.jobsapplied-1>p {
-    margin-left: 33px;
+.jobsapplied-1>* {
+    margin-left: 20px;
     font-size: 20px;
     font-weight: 600;
+    text-decoration: none;
+    color: #040404;
 }
 
 
